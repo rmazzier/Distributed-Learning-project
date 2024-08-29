6a544c4b2baf9786fa2caff654611dd06c7b541d
Riccardo Mazzieri <riccardo.mazzieri.music@gmail.com>
2024-08-29 20:14:04 +0200

    configured test simulations

--- simulations.py
+++ simulations.py
@@ -4,15 +4,19 @@ if __name__ == "__main__":
     from fl_simulator import Server, Simulation, FedAvg, GIANT
     from constants import CONFIG
 
-    # Global parameters, constant through different simulations
+    # This is For FedAVG.
+    # For GIANT we must divide this by two
+    NUM_ROUNDS = 10
+    N_BYZ = 1
 
-    CONFIG["N_CLIENTS"] = 10
-    CONFIG["NUM_ROUNDS"] = 50
-    CONFIG["MAX_CLIENT_ITERS"] = None
+    # Global parameters, constant through different simulations
+    CONFIG["N_CLIENTS"] = 3
+    CONFIG["MAX_CLIENT_ITERS"] = 50
 
     ###### 1 - Run the simulation with the GIANT strategy, NO ATTACKERS ######
+    CONFIG["NUM_ROUNDS"] = NUM_ROUNDS // 2
     CONFIG["N_BYZANTINE_CLIENTS"] = 0
-    CONFIG["MODEL_NAME"] = "GIANT_10C"
+    CONFIG["MODEL_NAME"] = f"GIANT_{CONFIG['N_CLIENTS']}C"
 
     strategy = GIANT(CONFIG["MAX_CLIENT_ITERS"])
     server = Server(CONFIG, reduce_op="mean")
@@ -23,7 +27,8 @@ if __name__ == "__main__":
     sim.start_simulation(n_rounds=CONFIG["NUM_ROUNDS"])
 
     ###### 2 - Run the same simulation, but with the FedAvg strategy ######
-    CONFIG["MODEL_NAME"] = "FedAvg_10C"
+    CONFIG["NUM_ROUNDS"] = NUM_ROUNDS
+    CONFIG["MODEL_NAME"] = f"FedAvg_{CONFIG['N_CLIENTS']}C"
 
     strategy = FedAvg(CONFIG["MAX_CLIENT_ITERS"])
     server = Server(CONFIG, reduce_op="mean")
@@ -33,9 +38,10 @@ if __name__ == "__main__":
     )
     sim.start_simulation(n_rounds=CONFIG["NUM_ROUNDS"])
 
-    ###### 3 - Run the simulation with the GIANT strategy, 4 attackers ######
-    CONFIG["N_BYZANTINE_CLIENTS"] = 4
-    CONFIG["MODEL_NAME"] = "GIANT_10C_4A"
+    ###### 3 - Run the simulation with the GIANT strategy, with attackers ######
+    CONFIG["NUM_ROUNDS"] = NUM_ROUNDS // 2
+    CONFIG["N_BYZANTINE_CLIENTS"] = N_BYZ
+    CONFIG["MODEL_NAME"] = f"GIANT_{CONFIG['N_CLIENTS']}C_{CONFIG['N_BYZANTINE_CLIENTS']}A"
 
     strategy = GIANT(CONFIG["MAX_CLIENT_ITERS"])
     server = Server(CONFIG, reduce_op="mean")
@@ -47,7 +53,8 @@ if __name__ == "__main__":
     sim.start_simulation(n_rounds=CONFIG["NUM_ROUNDS"])
 
     ###### 4 - Run the same simulation, but with the FedAvg strategy ######
-    CONFIG["MODEL_NAME"] = "FedAvg_10C_4A"
+    CONFIG["NUM_ROUNDS"] = NUM_ROUNDS
+    CONFIG["MODEL_NAME"] = f"FedAvg_{CONFIG['N_CLIENTS']}C_{CONFIG['N_BYZANTINE_CLIENTS']}A"
 
     strategy = FedAvg(CONFIG["MAX_CLIENT_ITERS"])
     server = Server(CONFIG, reduce_op="mean")
@@ -59,7 +66,8 @@ if __name__ == "__main__":
     sim.start_simulation(n_rounds=CONFIG["NUM_ROUNDS"])
 
     ###### 5 - Run the simulation with the GIANT strategy + Median aggregation (MNM) ######
-    CONFIG["MODEL_NAME"] = "GIANT_10C_4A_MNM"
+    CONFIG["NUM_ROUNDS"] = NUM_ROUNDS // 2
+    CONFIG["MODEL_NAME"] = f"GIANT_{CONFIG['N_CLIENTS']}C_{CONFIG['N_BYZANTINE_CLIENTS']}A_MNM"
 
     strategy = GIANT(CONFIG["MAX_CLIENT_ITERS"])
     server = Server(CONFIG, reduce_op="median")
